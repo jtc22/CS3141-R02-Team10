b4a77b2e84dea444642ee9534988fcaefbf5a267
dmdimitr <dmdimitr@mtu.edu>
2022-11-09 12:15:19 -0500

    using static materialproperties

--- CaveSimulator/Assets/Scripts/CaveSimulation.cs
+++ CaveSimulator/Assets/Scripts/CaveSimulation.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MaterialProperties;
+using static MaterialProperties;
 
 public class CaveSimulation : MonoBehaviour
 {
--- CaveSimulator/Assets/Scripts/MaterialProperties.cs
+++ CaveSimulator/Assets/Scripts/MaterialProperties.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public static class MaterialProperties 
 {
     public enum Material {
