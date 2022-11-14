02cfeca7eca9764045ddf9c646be1e4b58b6b68c
jtcolwell <jtcolwell22@gmail.com>
2022-11-14 12:38:44 -0500

    Added material hardnesses

--- CaveSimulator/Assets/Scripts/MaterialProperties.cs
+++ CaveSimulator/Assets/Scripts/MaterialProperties.cs
@@ -29,35 +29,35 @@ public static class MaterialProperties
                 case (Material.basalt):
                     prop.name = "Basalt";
                     prop.density = 3f;
-                    prop.hardness = 1;
+                    prop.hardness = 5.5f;
                     prop.solubility = 1;
                     prop.color = new Color(.380f, .412f, .424f, 1);
                     return prop;
                 case (Material.limestone):
                     prop.name = "Limestone";
                     prop.density = 2f; // 1.5-2.71 g/cm^3
-                    prop.hardness = 1;
+                    prop.hardness = 3.5f;
                     prop.solubility = 1;
                     prop.color = new Color(0.74f, 0.74f, 0.56f, 1);
                     return prop;
                 case (Material.sandstone):
                     prop.name = "Sandstone";
                     prop.density = 2.3f; // 2.0-2.6
-                    prop.hardness = 1;
+                    prop.hardness = 6.5f;
                     prop.solubility = 1;
                     prop.color = new Color(.698f, .565f, .510f, 1);
                     return prop;
                 case (Material.dolomite):
                     prop.name = "Dolomite";
                     prop.density = 2.84f; // Avg
-                    prop.hardness = 1;
+                    prop.hardness = 3.75f;
                     prop.solubility = 1;
                     prop.color = new Color(.714f, .702f, .671f, 1);
                     return prop;
                 case (Material.granite):
                     prop.name = "Granite";
                     prop.density = 2.75f; // 2.7-2.8
-                    prop.hardness = 1;
+                    prop.hardness = 7f;
                     prop.solubility = 1;
                     prop.color = new Color(.404f, .404f, .404f, 1);
                     return prop;
